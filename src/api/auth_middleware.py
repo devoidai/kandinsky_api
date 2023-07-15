2ceffa427524347337aa71ece55db1b51158c0d2
aptemmaleev <maleev2002@yandex.ru>
2023-07-15 11:49:32 +0300

    Pass homepage and docs without token

--- src/api/auth_middleware.py
+++ src/api/auth_middleware.py
@@ -9,6 +9,16 @@ class AuthMiddleware:
         
     async def __call__(self, request: Request, call_next):
         '''Verifies the token in the request header'''
+        methods = str(request.url).replace(str(request.base_url), '')
+        # Pass homepage
+        if methods == '':
+            response = await call_next(request)
+            return response
+        # Pass docs
+        if methods in ('docs', 'openapi.json', 'favicon.ico'):
+            response = await call_next(request)
+            return response    
+        # Authorization check
         try:
             token = request.headers["authorization"]
             if self.token == token:
